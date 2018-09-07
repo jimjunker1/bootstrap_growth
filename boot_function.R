e01ab3af29a0cd09ac2f58e39912e9ae096a9bee
jimjunker1 <james.junker@msu.montana.edu>
2018-09-06 20:55:50 -0600

    function done for single taxon.

--- boot_function.R
+++ boot_function.R
@@ -1,6 +1,7 @@
 #boot strap function
 
 cohort_boot = function(DATA,nboot = NULL,...){
+  #sets the number of individuals you want to sample
   if(is.null(nboot)){
     nboot = 500
   } else{ nboot = nboot }
@@ -12,46 +13,57 @@ for(i in taxa){
 for(j in sites){
   site_data = tax_data[which(as.factor(tax_data$Site) == j),]#grab all data for a single site
 
-  #convert to dates and calculate julian day
+#convert to dates and calculate julian day
 site_data$Pd = as.POSIXct(site_data$date, format = "%d-%m-%y")
 site_data$date = as.factor(site_data$Pd)
 site_data$julian = as.numeric(format(site_data$Pd, "%j"));site_data$Pd = NULL
-  #reorder dates based start of cohort
+
+#reorder dates based start of cohort
 site_data %>%
   group_by(date) %>%
-  summarize(mean_mass = mean(mass, na.rm=T), julian = unique(julian, na.rm = T)) -> date_df
-date_mass = within(date_df, date <- reorder(date, mean_mass))
+  summarize(mean_mass = mean(mass, na.rm=T), julian = unique(julian, na.rm = T)) %>%
+  arrange(mean_mass) %>%
+  mutate(date = reorder(date, mean_mass)) %>%
+  mutate(day = c(diff(julian),NA), id = 1:n()) -> date_df
+
+#fix the weird wrap around dates
+wrap_loc = which(date_df$day < 0);wrap_fix = 365-abs(date_df[which(date_df$day < 0),'day'])
+date_df[wrap_loc, 'day'] = wrap_fix
+
+#reorder the site data based on 'cohort date'
 site_data$date = factor(site_data$date, levels = levels(date_df$date))
-source("./bootstrap_function.R")
+
+source("./bootstrap_function.R")#function selects data 
 bootsdata = boots(site_data, nboot = nboot)
 bootsdata = bootsdata[-1,]
-browser()
 
 bootsdata %>%
-  select(species, date, mass) %>%
+  select(species, Site, date, mass) %>%
+  left_join(date_df[,c('date','id')]) -> bootsdata_id
+
+bootsdata %>%
+  select(species, Site, date, mass) %>%
   mutate(id = rep(1:nboot, nrow(date_df))) %>%
   spread(date,mass) %>%
-  select(-id)-> bootsdata.wide
+  select(-id)-> bootsdata_wide
 
+#now work across two columns at a time to estimate growth
+#1:names(bootsdata[,-1:2]))
+growth_rate <- function(x,y,z) {(log(x)/log(y))/z}
 
-igr = c()
-start_date = c()
-for(i in 1:unique(levels(bootsdata$date))){
- paste(i+1)
- igr_vec = mapply(function(w,x,y,z) (log(w)/log(x))/(z-y), bootsdata[which(bootsdata$date == i),'mass'], 
-                                                                         bootsdata[which(bootsdata$date == as.character(i+1)),'mass'],
-                    bootsdata[which(bootsdata$date == i),'julian'],bootsdata[which(bootsdata$date == as.character(i+1)),'julian'])
- i_date = rep(as.chracter(i), nboot)
- 
- igr = rbind(igr, igr_vec)
- start_date = rbind(start_date, i_date)
-} 
-  
+igr_df = c()
+for(w in 1:(max(bootsdata_id$id)-1)){
+  igr_vec = mapply(growth_rate, bootsdata_id[which(bootsdata_id$id == w),'mass'], bootsdata_id[which(bootsdata_id$id == w+1),'mass'],
+                   date_df[w,'day'])
+  igr_df = cbind(igr_df,igr_vec)
+}
 
-mapply(function(x,y) (log(mass[i])/log(mass[i+1]))/(julian[i+1]-julian[i]))
-  
+colnames(igr_df) = unique(levels(date_df$date))[1:(nrow(date_df)-1)]
+igr_df = data.frame(species = as.character(i), site = j, igr_df, check.names = F)
+igr_df %>%
+  gather(start_date, IGR, 3:(dim(igr_df)[2]), factor_key = T) -> igr_long
 
-
+write.csv(igr_long, file = paste(i,"_site-",j,"_IGR.csv",sep = ""), row.names = F) 
   }  
  }  
 }
--- test_script.R
+++ test_script.R
@@ -1,6 +1,6 @@
 # script to boot strap growth rates from size-frequency histograms
 library(tidyverse);theme_set(theme_bw())
-
+library(tictoc)
 
 ##### some basic QAQC of data for my benefit #####
 
@@ -17,38 +17,30 @@ df$Pd = as.POSIXct(df$date, format = "%d-%m-%y")
 #just a quick visualization
 ggplot(df, aes(x = Pd, y = mass, colour = as.factor(Site))) + 
   geom_point(size =2, position = "jitter")
+
+x = subset(df, Site == "1" & date == "19-11-17")
+hist(x$bodySize, breaks = 30)
+
+#quick notes: You should take a look at some of the size frequency data (see above).
+#It appears you potentially have multiple cohorts going at the same time.
+#you should probably split these out at some point, otherwise this code is going 
+#to do some weird stuff and you will get some wack-a-doo growth estimates
 ##### done with QAQC #####
 
 df = read.csv(file = "./infrequensMeasurements.csv",T)
-#df$Pd = as.POSIXct(df$date, format = "%d-%m-%y")
-#df$date = as.factor(df$Pd)
-#df$julian = as.numeric(format(df$Pd, format = "%j"));df$Pd = NULL
-
-
 
 #load in the function
 source("./boot_function.R")
-debugonce(cohort_boot)
-x = cohort_boot(df)
+#debugonce(cohort_boot)
+tic()
+suppressWarnings(cohort_boot(df))
+toc()
 
+#take a look at one example
+x = read.csv(file = "infrequens_site-1_IGR.csv",T)
 
-df %>%
-  group_by(Pd) %>%
-  summarize(mean_mass = mean(mass, na.rm = T)) -> date_mass
+x_1 = subset(x,site == 1 & start_date == "2017-08-18")
 
-date_mass = within(date_mass, Pd <- reorder(Pd, mean_mass))
-levels(date_mass$Pd)
-
-df$date = factor(df$date, levels = levels(date_mass$Pd))
-levels(df$date)
-
-bootsdata = subset(df, Site == 1)
-
-del.mass = mapply(function(x,y) (log(x)/log(y)), bootsdata[which(bootsdata$date == "17-09-17"),'mass'], 
-                  bootsdata[which(bootsdata$date == "15-10-17"),'mass'])
-
-for(i in 1:unique(levels(as.factor(df$date)))){
-  
-} 
-
-unique(levels(as.factor(df$date)))
+hist(x_1$IGR)
+median(x_1$IGR)
+quantile(x_1$IGR, c(0.05,0.95))
